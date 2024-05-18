57b63a5905f406eba3382dbe9bd2a20682a5b947
sumantamukhopadhyay <sumanta9090@gmail.com>
2024-05-19 04:56:37 +0530

    - Variable decleration.

--- InvestmentApp/invest_calculator.go
+++ InvestmentApp/invest_calculator.go
@@ -1,12 +1,19 @@
 package main
 
-import "math"
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+// Instead of calling varible like this: var investmentAmount float64 = 1000 you can call it like this: investmentAmount := 1000 this is shorthand for declaring and initializing a variable.
+// This is called short variable declaration.
+// Defining varibles in same line is called multi-variable declaration.
+// You can also define the type of the variable in the end like this: investmentAmount, expectedReturnRate, years := 1000, 5.5, 10
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	var investmentAmount float64 = 1000
+	var expectedReturnRate float64 = 5.5
+	var years float64 = 10
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	fmt.Println("Future value of investment is ", futureValue)
 }
