626e8f2031a801f2e237c7d66ffb3b55831f9886
sumantamukhopadhyay <sumanta9090@gmail.com>
2024-05-19 05:02:15 +0530

    - Added inflation rate in calculation.

--- InvestmentApp/invest_calculator.go
+++ InvestmentApp/invest_calculator.go
@@ -11,9 +11,27 @@ import (
 // You can also define the type of the variable in the end like this: investmentAmount, expectedReturnRate, years := 1000, 5.5, 10
 
 func main() {
+	// Declare and initialize the investment amount
 	var investmentAmount float64 = 1000
+
+	// Declare and initialize the expected return rate
 	var expectedReturnRate float64 = 5.5
+
+	// Declare and initialize the number of years
 	var years float64 = 10
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+
+	// Declare and initialize the inflation rate as a constant
+	const inflationRate float64 = 3.5
+
+	// Calculate the future value of the investment
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+
+	// Calculate the inflation-adjusted future value of the investment
+	inflationAdjustedFutureValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	// Print the future value of the investment
 	fmt.Println("Future value of investment is ", futureValue)
+
+	// Print the inflation-adjusted future value of the investment
+	fmt.Println("Inflation adjusted future value of investment is ", inflationAdjustedFutureValue)
 }
