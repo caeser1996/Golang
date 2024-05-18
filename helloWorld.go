0edd0c2e9ebdf3c478f1980db2456538fed6134c
sumantamukhopadhyay <sumanta9090@gmail.com>
2024-05-19 03:37:53 +0530

    - Hello world app.

---
+++ helloWorld.go
@@ -0,0 +1,13 @@
+// Type: Go Source Code
+// Description: A simple program that prints "Hello, World!" to the console
+package main
+
+// Import the fmt package
+// fmt is short for format and is used for formatting input and output
+import "fmt"
+
+func main() {
+	// Print "Hello, World!" to the console
+	// Println is short for print line
+	fmt.Println("Hello, World!")
+}
