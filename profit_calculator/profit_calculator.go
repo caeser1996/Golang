3eb87536ee91209c168b6ab4d76c5da567ea8337
sumantamukhopadhyay <sumanta9090@gmail.com>
2024-05-19 05:59:07 +0530

    - Profit calculator is added.

---
+++ profit_calculator/profit_calculator.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue float64 // Variable to store the revenue
+	var expenses float64 // Variable to store the expenses
+	var taxRate float64 // Variable to store the tax rate
+
+	fmt.Print("Enter the revenue: ") // Prompting the user to enter the revenue
+	fmt.Scan(&revenue) // Reading the revenue input from the user
+
+	fmt.Print("Enter the expenses: ") // Prompting the user to enter the expenses
+	fmt.Scan(&expenses) // Reading the expenses input from the user
+
+	fmt.Print("Enter the tax rate: ") // Prompting the user to enter the tax rate
+	fmt.Scan(&taxRate) // Reading the tax rate input from the user
+
+	ebt := revenue - expenses // Calculating the earnings before tax
+	profit := ebt * (1 - taxRate/100) // Calculating the profit after tax
+	ratio := ebt / profit // Calculating the ratio of earnings before tax to profit
+
+	fmt.Println("Earnings before tax is ", ebt) // Printing the earnings before tax
+	fmt.Println("Profit is ", profit) // Printing the profit
+	fmt.Println("Ratio is ", ratio) // Printing the ratio
+}
